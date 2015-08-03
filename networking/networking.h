65a8b86d5bf2da453dedca2dcfcaeed248ab2070
mbarjel <mbarjel@hotmail.com>
2015-08-03 16:54:01 +1000

    initial commit
    Signed-off-by: mbarjel <mbarjel@hotmail.com>

---
+++ networking/networking.h
@@ -0,0 +1,13 @@
+//
+//  networking.h
+//  networking
+//
+//  Created by Melad Barjel on 3/08/2015.
+//  Copyright (c) 2015 Snepo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface networking : NSObject
+
+@end
---
+++ networking/networking.m
@@ -0,0 +1,13 @@
+//
+//  networking.m
+//  networking
+//
+//  Created by Melad Barjel on 3/08/2015.
+//  Copyright (c) 2015 Snepo. All rights reserved.
+//
+
+#import "networking.h"
+
+@implementation networking
+
+@end
